ed249cb15eeb8af8c054442967e0962baa9c99c4
codeBYpRINCE-STACK <singh.prince.070824@gmail.com>
2026-02-19 17:17:40 +0530

    Add files via upload

---
+++ week1/Day4/problemAnalysisTool.js
@@ -0,0 +1,15 @@
+
+let number = 10;
+
+let result = (number >= 0) ? "Positive Number" : "Negative Number";
+console.log(result);
+
+if (number % 2 === 0) {
+    console.log("Even Number");
+} else {
+    console.log("Odd Number");
+}
+
+for (let i = 1; i <= number; i++) {
+    console.log(i);
+}
---
+++ week1/Day4/trafficSignalSimulator.js
@@ -0,0 +1,20 @@
+
+let signal = "orange";
+
+
+switch (signal) {
+    case "red":
+        console.log("Stop");
+        break;
+
+    case "yellow":
+        console.log("Get Ready");
+        break;
+
+    case "green":
+        console.log("Go");
+        break;
+
+    default:
+        console.log("Invalid Signal");
+}
