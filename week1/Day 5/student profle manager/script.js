2197784f4ea00f4d55f7322ab5215e0655cd413b
codeBYpRINCE-STACK <singh.prince.070824@gmail.com>
2026-02-20 17:55:57 +0530

    Update script.js

--- week1/Day 5/student profle manager/script.js
+++ week1/Day 5/student profle manager/script.js
@@ -1,17 +1,16 @@
-// Global student object
+
 var student = {
     name: "Prince",
     rollNo: 101,
     marks: 85
 };
 
-// DOM references
+
 var profileDiv = document.getElementById("studentProfile");
 var showProfileBtn = document.getElementById("showProfileBtn");
 var updateMarksBtn = document.getElementById("updateMarksBtn");
 var marksInput = document.getElementById("marksInput");
 
-// Function to display profile
 function updateStudentProfile(studentObj) {
     profileDiv.innerHTML =
         "<p><strong>Name:</strong> " + studentObj.name + "</p>" +
@@ -19,13 +18,12 @@ function updateStudentProfile(studentObj) {
         "<p><strong>Marks:</strong> " + studentObj.marks + "</p>";
 }
 
-// Function to update marks
 function updateMarks(newMarks) {
     student.marks = newMarks;
     updateStudentProfile(student);
 }
 
-// Event Listeners
+
 showProfileBtn.addEventListener("click", function () {
     updateStudentProfile(student);
 });
